998ce5c4b8839e68084110c0f929b49fdd5265da
dzianis danilkou <d.danilkov@dialmyapp.com>
2022-11-06 13:24:30 +0300

    wip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,13 +7,13 @@ export const environment: Config = {
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://xrrzo5hore.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://xwd18eaflg.execute-api.eu-west-1.amazonaws.com/dev',
-    cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    cart: 'http://dandzi-92-cart-api-dev.eu-west-1.elasticbeanstalk.com',
   },
   apiEndpointsEnabled: {
     product: false,
     order: false,
     import: true,
     bff: true,
-    cart: false,
+    cart: true,
   },
 };
